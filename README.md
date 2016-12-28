ffc97011980d2b7e1a42ff4c24369e05e67fad48
Bojan Djurkovic <dbojan@gmail.com>
2016-12-27 22:12:19 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<a name="module_mali-tojson"></a>
+
+### mali-tojson
+Mali <code>toJSON</code> middleware. If the response object has a <code>toJSON</code> function it's executed upon
+payload. Only applies for <code>UNARY</code> and <code>REQUEST_STREAM</code> call types.
+
+**Example**  
+
+```js
+const toJSON = require('mali-tojson')
+
+function handler(ctx) {
+  const obj = {
+    email: 'bob@gmail.com',
+    password: 'mysecret'
+  }
+
+  obj.toJSON = function() {
+    return {
+      email: this.email
+    }
+  }
+
+  ctx.res = obj // password will not be in the payload to client
+}
+
+app.use('fn', toJSON, handler)
+```
+
---
+++ index.js
@@ -0,0 +1,28 @@
+const xform = require('mali-transform')
+
+/**
+ * Mali <code>toJSON</code> middleware. If the response object has a <code>toJSON</code> function it's executed upon
+ * payload. Only applies for <code>UNARY</code> and <code>REQUEST_STREAM</code> call types.
+ * @module mali-tojson
+ *
+ * @example
+ * const toJSON = require('mali-tojson')
+ *
+ * function handler(ctx) {
+ *   const obj = {
+ *     email: 'bob@gmail.com',
+ *     password: 'mysecret'
+ *   }
+ *
+ *   obj.toJSON = function() {
+ *     return {
+ *       email: this.email
+ *     }
+ *   }
+ *
+ *   ctx.res = obj // password will not be in the payload to client
+ * }
+ *
+ * app.use('fn', toJSON, handler)
+ */
+module.exports = xform('toJSON')
